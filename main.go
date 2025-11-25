08bdf7ac7eae9ccada3c9367d23cee1f1efaac61
lutzpeschlow <lutz.peschlow@gmail.com>
2025-11-25 11:07:15 +0100

    example extended with listbox

--- main.go
+++ main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"fmt"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()                  // neue Fyne-Anwendung erstellen
-	w := a.NewWindow("Hello World") // neues Fenster mit Titel
+	a := app.New()
+	w := a.NewWindow("Hello")
+	items := []string{"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"}
+	len_items := len(items)
+	fmt.Print(" number of items: ", len_items)
 
-	w.SetContent(widget.NewLabel("Hello World!")) // Label als Inhalt setzen
+	list := widget.NewList(
+		func() int {
+			return len(items)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("")
+		},
+		func(i widget.ListItemID, obj fyne.CanvasObject) {
+			obj.(*widget.Label).SetText(items[i])
+		})
+
+	scroll := container.NewVScroll(list)
+	w.SetContent(scroll)
+
 	fmt.Println("Running")
+	w.Resize(fyne.NewSize(400, 200))
 
-	w.ShowAndRun() // Fenster anzeigen und Anwendung starten
+	w.ShowAndRun()
 }
